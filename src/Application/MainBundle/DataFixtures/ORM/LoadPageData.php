6181ff7eb1ffd3b88f3ff9433828894b9e219760
siciarek <siciarek@gmail.com>
2013-08-05 17:31:26 +0200

    default config update

--- src/Application/MainBundle/DataFixtures/ORM/LoadPageData.php
+++ src/Application/MainBundle/DataFixtures/ORM/LoadPageData.php
@@ -23,60 +23,9 @@ class LoadPageData extends BaseFixture
      */
     public function load(ObjectManager $om)
     {
-        return;
-
         $pages = array(
             array(
-                'title'   => 'Str. główna',
-                'home'    => true,
-                'enabled' => true,
-                'content' => file_get_contents(__DIR__ . '/../data/home.html')
-            ),
-            array(
-                'title'   => 'O konferencji',
-                'enabled' => true,
-                'content' => file_get_contents(__DIR__ . '/../data/about.html')
-            ),
-            array(
-                'title'   => 'Program',
-                'enabled' => true,
-                'content' => file_get_contents(__DIR__ . '/../data/agenda.html')
-            ),
-            array(
-                'title'   => 'Prelegenci',
-                'enabled' => true,
-                'content' => 'Zawartość tej strony jest generowana automatycznie. Można tylko zmienić jej pozycję w menu.',
-            ),
-            array(
-                'title'   => 'Cennik',
-                'enabled' => true,
-                'content' => ''
-                . "<h1>Cennik</h1>\n\n"
-                . file_get_contents(__DIR__ . '/../data/cennik/regular.html')
-                . file_get_contents(__DIR__ . '/../data/cennik/ipma.html')
-                . file_get_contents(__DIR__ . '/../data/cennik/student.html')
-                . '<div>&nbsp;</div>'
-                . file_get_contents(__DIR__ . '/../data/cennik/regulations.html')
-            ),
-            array(
-                'title'   => 'Zarejestruj się',
-                'enabled' => true,
-                'content' => 'Zawartość tej strony jest generowana automatycznie. Można tylko zmienić jej pozycję w menu.',
-            ),
-            array(
-                'title'   => 'Lokalizacja',
-                'enabled' => true,
-                'content' => file_get_contents(__DIR__ . '/../data/location.html')
-            ),
-
-            array(
-                'title'   => 'Kontakt',
-                'enabled' => true,
-                'content' => file_get_contents(__DIR__ . '/../data/contact.html')
-            ),
-
-            array(
-                'title'   => 'Polityka prywatności serwisu',
+                'title'   => 'Privacy policy',
                 'enabled' => true,
                 'content' => file_get_contents(__DIR__ . '/../data/privacypolicy.html')
             ),
