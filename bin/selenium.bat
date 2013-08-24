4383a16dab785c4a9d3ec9656c519a070b0efff5
Jacek Siciarek <siciarek@gmail.com>
2013-08-24 07:39:30 +0200

    linux.pl server adjust

--- bin/selenium.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-SET BIN_TARGET=%~dp0\"../vendor/siciarek/symfony-utils-bundle/bin/selenium"\selenium
-php "%BIN_TARGET%" %*
--- bin/xbehat.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-SET BIN_TARGET=%~dp0\"../vendor/siciarek/symfony-utils-bundle/bin/behat"\xbehat
-php "%BIN_TARGET%" %*
